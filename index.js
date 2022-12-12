bdf8ecc6575910115048156bed7e36806952c066
davitorress <dev.davitorres@gmail.com>
2022-12-12 16:13:43 -0300

    arithmetic operators

--- index.js
+++ index.js
@@ -47,3 +47,19 @@ function square(number) {
 
 greet("Davi", "Torres");
 console.log(square(2));
+
+// Arithmetic Operators
+let x = 10;
+let y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
+
+// Increment
+console.log(++x);
+// Decrement
+console.log(--x);
