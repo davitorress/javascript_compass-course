1335b2cef0679620ddec3959954fe796dc2a467d
davitorress <dev.davitorres@gmail.com>
2022-12-12 12:43:22 -0300

    variables and constant

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+// Variables
+
+// Rules
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number
+// Cannot contain a space or hyphen
+// Are case-sensitive
+
+let firstName = "Davi";
+console.log(firstName);
+
+const interestRate = 0.3;
+// Cannot change the value of a constant
+// interestRate = 1;
+console.log(interestRate);
