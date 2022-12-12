da9a8afeb975ea81c75c1f63f71884140d621fe2
davitorress <dev.davitorres@gmail.com>
2022-12-12 17:14:04 -0300

    switch...case

--- control_flow.js
+++ control_flow.js
@@ -4,3 +4,19 @@ let hour = 13;
 if (hour >= 6 && hour < 12) console.log("Good morning!");
 else if (hour >= 12 && hour < 18) console.log("Good afternoon!");
 else console.log("Good evening!");
+
+// Switch...case
+let role;
+
+switch (role) {
+	case "guest":
+		console.log("Guest User");
+		break;
+
+	case "moderator":
+		console.log("Moderator User");
+		break;
+
+	default:
+		console.log("Unknown User");
+}
