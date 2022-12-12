99c39ffa2c4af44f4676fbcc53e70d9aeb2a3f28
davitorress <dev.davitorres@gmail.com>
2022-12-12 16:05:18 -0300

    reference types

--- index.js
+++ index.js
@@ -21,3 +21,29 @@ let age = 18; // Number literal
 let isApproved = true; // Boolean literal
 let test; // Undefined
 let selectedColors = null; // Null
+
+// Reference Types
+let person = {
+	name: "Davi",
+	age: 18,
+}; // Object literal
+person.name = "Gabriel"; // Dot Notation
+person["name"] = "Vitoria"; // Bracket Notation
+console.log(person.name);
+
+selectedColors = ["green", "purple"]; // Array literal
+selectedColors[2] = 1;
+console.log(selectedColors);
+
+// Performing a task
+function greet(firstName, lastName) {
+	console.log("Hello " + firstName + " " + lastName);
+} // Function declaration
+
+// Returning a value
+function square(number) {
+	return number * number;
+}
+
+greet("Davi", "Torres");
+console.log(square(2));
