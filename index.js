5b76ea5f14e58e5d8bb133cd3cbb79fdeb6e35b9
davitorress <dev.davitorres@gmail.com>
2022-12-12 12:48:07 -0300

    primitive types

--- index.js
+++ index.js
@@ -14,3 +14,10 @@ const interestRate = 0.3;
 // Cannot change the value of a constant
 // interestRate = 1;
 console.log(interestRate);
+
+// Primitive Types
+let fullName = "Davi Torres"; // String
+let age = 18; // Number literal
+let isApproved = true; // Boolean literal
+let test; // Undefined
+let selectedColors = null; // Null
