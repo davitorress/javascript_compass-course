53c30339fe4d600580d3a551b886ae6c15855200
davitorress <dev.davitorres@gmail.com>
2022-12-13 13:54:47 -0300

    date object

--- objects.js
+++ objects.js
@@ -40,3 +40,10 @@ function Circle(radius) {
 }
 
 const circle3 = new Circle(3);
+
+// Date object
+const now = new Date();
+const date1 = new Date("May 11 2018 09:00");
+const date2 = new Date(2018, 4, 11, 9);
+
+date2.setFullYear(2019);
