017e9d892e18fd8ea525728e81772110835b15ff
davitorress <dev.davitorres@gmail.com>
2022-12-13 11:33:01 -0300

    exercise count truthy

---
+++ exercises/count_truthy.js
@@ -0,0 +1,9 @@
+function countTruthy(array) {
+	let count = 0;
+
+	for (let value of array) if (value) count++;
+	return count;
+}
+
+const array = [0, null, undefined, "", 2, 3, "text"];
+console.log(countTruthy(array));
