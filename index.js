b6828b5c5ec6e9242d8996a9bc3bfd78fdead71c
davitorress <dev.davitorres@gmail.com>
2022-12-13 17:57:18 -0300

    file organization

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Imports to see the console.log on the browser
 // the codes are running using node
 
-// import "./basics.js";
-// import "./operators.js";
-// import "./control_flow.js";
-// import "./objects.js";
-// import "./arrays.js";
-// import "./functions.js";
+// import "./topics/basics.js";
+// import "./topics/operators.js";
+// import "./topics/control_flow.js";
+// import "./topics/objects.js";
+// import "./topics/arrays.js";
+// import "./topics/functions.js";
